22fbc3070c6f249c202d3a4222ea5a7cc3f7c158
jepl-towquest <8996303+JamesPaganLodge@users.noreply.github.com>
2018-11-09 14:22:38 +0000

    array manipulation with pop()

--- JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/arrays.js
+++ JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/arrays.js
@@ -33,3 +33,9 @@ ourArray.push(["happy", "joy"]);
 var myArray = [["John", 23], ["cat", 2]];
 myArray.push(["dog", 3]);
 // ------------------------------------------
+// Array manipulation: pop()
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop();
+var myArray = [["John", 23], ["cat", 2]];
+var removedFromMyArray = myArray.pop();
+// ------------------------------------------
