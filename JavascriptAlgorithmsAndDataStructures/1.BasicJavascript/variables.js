56f9b12124c981666d82e0ddce2f25c5236e6d46
jepl-towquest <8996303+JamesPaganLodge@users.noreply.github.com>
2018-11-09 11:21:04 +0000

    Initializing uninitialized variables

--- JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/variables.js
+++ JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/variables.js
@@ -13,3 +13,12 @@ b = a;
 
 // Initializing variables
 var a = 9;
+
+// Uninitialized  variables are undefined
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1; // a = 6
+b = b + 5; // b = 15
+c = c + " String!"; // c = "I am a String!"
