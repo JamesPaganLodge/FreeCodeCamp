4bd5708c64ff268fb810a09959ec8aaa9b0175f1
jepl-towquest <8996303+JamesPaganLodge@users.noreply.github.com>
2018-11-09 15:45:32 +0000

    comparison with < operatoir

--- JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/booleans.js
+++ JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/booleans.js
@@ -130,3 +130,23 @@ console.log(testGreaterOrEqual(19));
 console.log(testGreaterOrEqual(100));
 console.log(testGreaterOrEqual(21));
 // ------------------------------------------
+// Comparison with less than operator
+function testLessThan(val) {
+  if (val < 25) {
+    return "Under 25";
+  }
+
+  if (val < 55) {
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+console.log("\nLess than");
+console.log(testLessThan(0));
+console.log(testLessThan(24));
+console.log(testLessThan(25));
+console.log(testLessThan(54));
+console.log(testLessThan(55));
+console.log(testLessThan(99));
+// ------------------------------------------
