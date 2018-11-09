a431cc73e8ea8ca53e21a9d8e05b520d3da75fa6
jepl-towquest <8996303+JamesPaganLodge@users.noreply.github.com>
2018-11-09 15:17:24 +0000

    conditional logic with if statements

--- JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/booleans.js
+++ JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/booleans.js
@@ -1,3 +1,25 @@
 function welcomeToBooleans() {
   return true;
 }
+
+// Conditional logic with if statements
+function ourTrueOrFalse(isItTrue) {
+  if (isItTrue) {
+    return "Yes, it's true";
+  }
+
+  return "No, it's false";
+}
+console.log(ourTrueOrFalse(true));
+console.log(ourTrueOrFalse(false));
+
+function trueOrFalse(wasThatTrue) {
+  if (wasThatTrue) {
+    return "Yes, that was true";
+  }
+
+  return "No, that was false";
+}
+console.log(trueOrFalse(true));
+console.log(trueOrFalse(false));
+// ------------------------------------------
