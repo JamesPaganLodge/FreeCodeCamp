1510b2032ef0ee314e7153437538600cfe5f7492
jepl-towquest <8996303+JamesPaganLodge@users.noreply.github.com>
2018-11-09 14:20:19 +0000

    array manipulation with push()

--- JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/arrays.js
+++ JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/arrays.js
@@ -26,3 +26,10 @@ myArray[0] = 45;
 var myArray = [[1,2,3],[4,5,6],[7,8,9],[[10,11,12],13,14]];
 var myData = myArray[2][1]; // access the value 8
 // ------------------------------------------
+// Array manipulation: push()
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]);
+// ourArray = ["Stimpson", "J", "cat", ["happy", "joy"]]
+var myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3]);
+// ------------------------------------------
