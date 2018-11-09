4850761551e437221b7de12246a420c9bffde997
jepl-towquest <8996303+JamesPaganLodge@users.noreply.github.com>
2018-11-09 15:38:35 +0000

    comparison with greater than operator >

--- JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/booleans.js
+++ JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/booleans.js
@@ -88,3 +88,24 @@ console.log(testStrictNotEqual("17"));
 console.log(testStrictNotEqual(12));
 console.log(testStrictNotEqual("bob"));
 // ------------------------------------------
+// Comparison with greater than operator
+function testGreaterThan(val) {
+  if (val > 100) {
+    return "Over 100";
+  }
+
+  if (val > 10) {
+    return "Over 10";
+  }
+
+  return "10 or under";
+}
+console.log("\nGreater Than");
+console.log(testGreaterThan(10));
+console.log(testGreaterThan(0));
+console.log(testGreaterThan(11));
+console.log(testGreaterThan(99));
+console.log(testGreaterThan(100));
+console.log(testGreaterThan(101));
+console.log(testGreaterThan(150));
+// ------------------------------------------
