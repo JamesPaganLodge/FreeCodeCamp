06f401a653c7353d6c912b311c0388378cb6d5c3
jepl-towquest <8996303+JamesPaganLodge@users.noreply.github.com>
2018-11-09 11:40:20 +0000

    subtract 2 numbers

--- JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/math.js
+++ JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/math.js
@@ -5,4 +5,10 @@ var sum = 10 + 0;
 // Answer
 var sum = 10 + 10;
 // ------------------------------------------
-// 
+// Subtract 2 numbers
+// Setup
+var difference = 45 - 0;
+
+// Answer
+var difference = 45 - 33;
+// ------------------------------------------
