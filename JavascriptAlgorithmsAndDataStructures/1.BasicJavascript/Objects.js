f31eea4e9de7561b2672c71052c058ba0595bf28
jepl-towquest <8996303+JamesPaganLodge@users.noreply.github.com>
2018-11-09 17:25:11 +0000

    create myDog object

--- JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/Objects.js
+++ JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/Objects.js
@@ -5,3 +5,11 @@ var ourDog = {
   "friends": ["everything!"]
 };
 // ------------------------------------------
+// Building javascript objects
+var myDog = {
+  "name": "Tiber",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Daisy", "Sandy"]
+};
+// ------------------------------------------
