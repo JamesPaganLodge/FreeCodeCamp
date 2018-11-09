60c29ca53b8ad0375a9d150dc62bbf6144e8d231
jepl-towquest <8996303+JamesPaganLodge@users.noreply.github.com>
2018-11-09 15:53:39 +0000

    logical AND && operator

--- JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/booleans.js
+++ JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/booleans.js
@@ -171,3 +171,21 @@ console.log(testLessOrEqual(24));
 console.log(testLessOrEqual(25));
 console.log(testLessOrEqual(55));
 // ------------------------------------------
+// Logical AND && operator
+function testLogicalAnd(val) {
+  if (val <= 50 && val >= 25) {
+    return "Yes";
+  }
+
+  return "No";
+}
+console.log("\nLogical AND");
+console.log(testLogicalAnd(0));
+console.log(testLogicalAnd(24));
+console.log(testLogicalAnd(25));
+console.log(testLogicalAnd(30));
+console.log(testLogicalAnd(50));
+console.log(testLogicalAnd(51));
+console.log(testLogicalAnd(75));
+console.log(testLogicalAnd(80));
+// ------------------------------------------
