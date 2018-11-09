c9fa0f547cbd506d2777baba1d88460fc28a37db
jepl-towquest <8996303+JamesPaganLodge@users.noreply.github.com>
2018-11-09 16:40:37 +0000

    add default option to switch statement

--- JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/switch.js
+++ JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/switch.js
@@ -24,3 +24,29 @@ console.log(caseInSwitch(2));
 console.log(caseInSwitch(3));
 console.log(caseInSwitch(4));
 // ------------------------------------------
+// Switch: default option
+function switchOfStuff(val) {
+  var answer = "";
+  switch(val) {
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
+    default:
+      answer = "stuff";
+      break;
+  }
+  return answer;
+}
+console.log("\nDefault option");
+console.log(switchOfStuff("a"));
+console.log(switchOfStuff("b"));
+console.log(switchOfStuff("c"));
+console.log(switchOfStuff("d"));
+console.log(switchOfStuff(4));
+// ------------------------------------------
