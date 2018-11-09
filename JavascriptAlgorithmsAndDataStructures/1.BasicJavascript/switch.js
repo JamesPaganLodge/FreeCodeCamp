70599e0246d2c445138230de4a561e9b411c07ad
jepl-towquest <8996303+JamesPaganLodge@users.noreply.github.com>
2018-11-09 16:50:30 +0000

    replace if/else chain with switch

--- JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/switch.js
+++ JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/switch.js
@@ -83,3 +83,46 @@ console.log(sequentialSizes(7));
 console.log(sequentialSizes(8));
 console.log(sequentialSizes(9));
 // ------------------------------------------
+// Replace if/else with switch
+function chaingToSwitch(val) {
+  var answer = ""
+  // if (val === "bob") {
+  //   answer = "Marley";
+  // } else if (val === 42) {
+  //   answer = "The Answer";
+  // } else if (val === 1) {
+  //   answer = "There is no #1";
+  // } else if (val === 99) {
+  //   answer = "Missed me by this much!";
+  // } else if (val === 7) {
+  //   answer = "Ate Nine";
+  // }
+  switch(val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+
+  return answer;
+}
+console.log("\nIf/else chain to switch");
+console.log(chaingToSwitch("bob"));
+console.log(chaingToSwitch(42));
+console.log(chaingToSwitch(1));
+console.log(chaingToSwitch(99));
+console.log(chaingToSwitch(7));
+console.log(chaingToSwitch("John"));
+console.log(chaingToSwitch(156));
+// ------------------------------------------
