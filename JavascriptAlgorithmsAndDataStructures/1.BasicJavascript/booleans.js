6e53098f528d5f96e5cb94c472565e67709a7fc9
jepl-towquest <8996303+JamesPaganLodge@users.noreply.github.com>
2018-11-09 16:20:51 +0000

    chaining if/else statements

--- JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/booleans.js
+++ JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/booleans.js
@@ -257,3 +257,29 @@ console.log(orderMyLogic(4));
 console.log(orderMyLogic(6));
 console.log(orderMyLogic(11));
 // ------------------------------------------
+// Chaining if else statements
+function testSize(num) {
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else if (num >= 20) {
+    return "Huge";
+  }
+}
+console.log("\nChaining if/else statements");
+console.log(testSize(0));
+console.log(testSize(4));
+console.log(testSize(5));
+console.log(testSize(8));
+console.log(testSize(10));
+console.log(testSize(14));
+console.log(testSize(15));
+console.log(testSize(17));
+console.log(testSize(20));
+console.log(testSize(25));
+// ------------------------------------------
