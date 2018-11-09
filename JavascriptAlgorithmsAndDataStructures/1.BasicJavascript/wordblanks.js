0e5082d8433e9c1195edcf82b3ad2332f17aaa42
jepl-towquest <8996303+JamesPaganLodge@users.noreply.github.com>
2018-11-09 13:49:47 +0000

    complete the wordBlanks function

--- JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/wordblanks.js
+++ JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/wordblanks.js
@@ -1,7 +1,8 @@
 function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
-  var result = "";
+  var result = "In the " + myAdjective + " park my " + myNoun + "jumped over a child and " + myVerb + " off " + myAdverb + ".";
 
   return result;
 }
 
 wordBlanks("dog", "big", "ran", "quickly");
+wordBlanks("cat", "little", "hit", "slowly");
