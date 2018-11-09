8748524877b88fca9b79d4f51aa89f768630ea72
jepl-towquest <8996303+JamesPaganLodge@users.noreply.github.com>
2018-11-09 11:31:19 +0000

    Splitting the lessons up

--- JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/variables.js
+++ JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/variables.js
@@ -2,7 +2,7 @@
 var ourName;
 
 var myName;
-
+// ------------------------------------------
 // Storing values with variables
 // Setup
 var a;
@@ -10,10 +10,10 @@ var b = 2;
 
 a = 7;
 b = a;
-
+// ------------------------------------------
 // Initializing variables
 var a = 9;
-
+// ------------------------------------------
 // Uninitialized variables are undefined
 var a = 5;
 var b = 10;
@@ -22,7 +22,7 @@ var c = "I am a";
 a = a + 1; // a = 6
 b = b + 5; // b = 15
 c = c + " String!"; // c = "I am a String!"
-
+// ------------------------------------------
 // variable case sensitivity
 // Declarations before
 var StUdLyCapVaR;
@@ -43,3 +43,4 @@ var titleCaseOver;
 studlyCapVar = 10;
 properCamelCase = "A String";
 titleCaseOver = 9000;
+// ------------------------------------------
