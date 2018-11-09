16a4fb2b6e97edc983ed1cf40534ea3e892d4c9a
jepl-towquest <8996303+JamesPaganLodge@users.noreply.github.com>
2018-11-09 11:29:31 +0000

    fix case of variables

--- JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/variables.js
+++ JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/variables.js
@@ -14,7 +14,7 @@ b = a;
 // Initializing variables
 var a = 9;
 
-// Uninitialized  variables are undefined
+// Uninitialized variables are undefined
 var a = 5;
 var b = 10;
 var c = "I am a";
@@ -22,3 +22,24 @@ var c = "I am a";
 a = a + 1; // a = 6
 b = b + 5; // b = 15
 c = c + " String!"; // c = "I am a String!"
+
+// variable case sensitivity
+// Declarations before
+var StUdLyCapVaR;
+var properCamelCase;
+var TitleCaseOver;
+
+// Assignments before
+STUDLYCAPVAR = 10;
+PRoperCAmelCAse = "A String";
+tITLEcASEoVER = 9000;
+
+// Declarations after
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments after
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
