d7ee22336ac823f48f3daa219d342131617c0356
jepl-towquest <8996303+JamesPaganLodge@users.noreply.github.com>
2018-11-09 14:07:13 +0000

    nested arrays

--- JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/arrays.js
+++ JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/arrays.js
@@ -4,3 +4,7 @@ var ourArray = ["John", 23];
 // Multiple values in one variable
 var myArray = ["Jepl", 32];
 // ------------------------------------------
+// Nested arrays
+var ourArray = [["the universe", 42], ["everything", 101010]];
+var myArray = [["this array"], ["is in another"]];
+// ------------------------------------------
