3a9c308c024f31522c76bae2f0a3455a7c506d02
jepl-towquest <8996303+JamesPaganLodge@users.noreply.github.com>
2018-11-09 14:25:18 +0000

    array manipulation with shift()

--- JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/arrays.js
+++ JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/arrays.js
@@ -27,6 +27,7 @@ var myArray = [[1,2,3],[4,5,6],[7,8,9],[[10,11,12],13,14]];
 var myData = myArray[2][1]; // access the value 8
 // ------------------------------------------
 // Array manipulation: push()
+// add to the end of array
 var ourArray = ["Stimpson", "J", "cat"];
 ourArray.push(["happy", "joy"]);
 // ourArray = ["Stimpson", "J", "cat", ["happy", "joy"]]
@@ -34,8 +35,16 @@ var myArray = [["John", 23], ["cat", 2]];
 myArray.push(["dog", 3]);
 // ------------------------------------------
 // Array manipulation: pop()
+// removes last element of array
 var ourArray = [1,2,3];
 var removedFromOurArray = ourArray.pop();
 var myArray = [["John", 23], ["cat", 2]];
 var removedFromMyArray = myArray.pop();
+// ------------------------------------------
+// Arra manipulation: shift()
+// removes first element of array
+var ourArray = ["Stimpson", "J", ["cat"]];
+var removedFromOurArray = ourArray.shift();
+var myArray = [["John", 23], ["dog", 3]];
+var removedFromMyArray = myArray.shift();
 // ------------------------------------------
