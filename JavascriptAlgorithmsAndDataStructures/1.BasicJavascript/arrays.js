5368aab53149947cc63105f47ad368aababb3231
jepl-towquest <8996303+JamesPaganLodge@users.noreply.github.com>
2018-11-09 14:09:20 +0000

    access arrays using indexes

--- JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/arrays.js
+++ JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/arrays.js
@@ -8,3 +8,10 @@ var myArray = ["Jepl", 32];
 var ourArray = [["the universe", 42], ["everything", 101010]];
 var myArray = [["this array"], ["is in another"]];
 // ------------------------------------------
+// Access arrays with indexes
+var ourArray = [50,60,70];
+var ourData = ourArray[0];
+
+var myArray = [50,60,70];
+var myData = myArray[0];
+// ------------------------------------------
