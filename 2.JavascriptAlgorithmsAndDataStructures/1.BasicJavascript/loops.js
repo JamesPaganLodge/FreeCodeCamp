892f9952111af6126b4032eb5054d68ab7f4ce09
James Pagan-Lodge <stixx_60@hotmail.com>
2018-11-30 16:20:26 +0000

    add while loop solution to push values 0-4 to an array

--- 2.JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/loops.js
+++ 2.JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/loops.js
@@ -1,3 +1,11 @@
 // Iterate with while loops
 var myArray = [];
+
+// push the values 0,1,2,3,4 to the array
+var i = 0;
+
+while(i < 5) {
+  myArray.push(i);
+  i++;
+}
 // ------------------------------------------
