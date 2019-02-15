88c75501505117bf0983935d191e8ee87b56bcef
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-02-15 14:58:15 +0000

    Using the conditional (ternary) operator

--- 2.JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/ternary.js
+++ 2.JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/ternary.js
@@ -1,6 +1,8 @@
 // using the conditional (ternary) operator
 function checkEqual(a, b) {
-
+  return a == b ? true : false;
 }
 
 console.log(checkEqual(1, 2));
+console.log(checkEqual(1, 1));
+console.log(checkEqual(1, -1));
