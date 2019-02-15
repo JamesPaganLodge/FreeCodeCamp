cc7647c226b5c64f6124ffba976ab307ff68886e
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-02-15 16:59:19 +0000

    set default parameters in a function

--- 2.JavascriptAlgorithmsAndDataStructures/2.ES6/arrow.js
+++ 2.JavascriptAlgorithmsAndDataStructures/2.ES6/arrow.js
@@ -24,4 +24,15 @@ const squareList = (arr) => {
 }
 
 const squaredIntegers = squareList(realNumberArray);
-console.log(squaredIntegers);
+//console.log(squaredIntegers);
+// ----------------------------------------------
+// Set function default parameters
+const increment = (function() {
+  'use strict';
+  return function increment(number, value = 1) {
+    return number + value;
+  }
+})();
+
+console.log(increment(5, 2));
+console.log(increment(5));
