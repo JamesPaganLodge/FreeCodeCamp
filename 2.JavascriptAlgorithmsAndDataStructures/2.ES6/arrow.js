c32bea5737509fb74712f588ee333258a7160dc2
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-02-15 16:33:02 +0000

    Arrow functions with parameters

--- 2.JavascriptAlgorithmsAndDataStructures/2.ES6/arrow.js
+++ 2.JavascriptAlgorithmsAndDataStructures/2.ES6/arrow.js
@@ -5,3 +5,12 @@ var magic = function() {
 }
 
 const MAGIC = () => new Date();
+// ----------------------------------------------
+// Arrow functions with parameters
+var myConcat = function(arr1, arr2) {
+  'use strict';
+  return arr1.concat(arr2);
+}
+
+const MYCONCAT = (arr1, arr2) => arr1.concat(arr2);
+console.log(MYCONCAT([1, 2], [3, 4, 5]));
