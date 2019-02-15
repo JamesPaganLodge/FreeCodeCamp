6311e35bcf365108b743673dfe5d775ffe90eb07
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-02-15 17:21:35 +0000

    Initial commit

---
+++ 2.JavascriptAlgorithmsAndDataStructures/2.ES6/destruct.js
@@ -0,0 +1,14 @@
+// Destructuring assignment to assign variables from objects
+const AVG_TEMPERATURE = {
+  today: 77.5,
+  tomorrow: 79
+};
+
+function getTempOfTmrw(avgTemperatures) {
+  'use strict';
+  const tempOfTomorrow = undefined;
+
+  return tempOfTomorrow;
+}
+
+console.log(getTempOfTmrw(AVG_TEMPERATURE));
