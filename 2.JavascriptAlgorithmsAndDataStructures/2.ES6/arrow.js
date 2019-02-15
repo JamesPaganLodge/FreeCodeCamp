6dff7bd6d564f6874f5271bb0adcaee40db68425
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-02-15 16:53:47 +0000

    Chaining arrow functions in higher order functions

--- 2.JavascriptAlgorithmsAndDataStructures/2.ES6/arrow.js
+++ 2.JavascriptAlgorithmsAndDataStructures/2.ES6/arrow.js
@@ -13,4 +13,15 @@ var myConcat = function(arr1, arr2) {
 }
 
 const MYCONCAT = (arr1, arr2) => arr1.concat(arr2);
-console.log(MYCONCAT([1, 2], [3, 4, 5]));
+//console.log(MYCONCAT([1, 2], [3, 4, 5]));
+// ----------------------------------------------
+// Higher order arrow functions
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+const squareList = (arr) => {
+  'use strict';
+  const squaredIntegers = arr.filter((num) => num > 0 && num % parseInt(num) == 0).map((num) => Math.pow(num, 2));
+  return squaredIntegers;
+}
+
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
