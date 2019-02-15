f79967c3b02740fc2e54c1c5585dec6f1cef9de1
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-02-15 11:38:43 +0000

    Iterate through an array with a for loop

--- 2.JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/loops.js
+++ 2.JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/loops.js
@@ -29,5 +29,13 @@ var myArray = [];
 for (var i = 9; i > 0; i -= 2) {
   myArray.push(i);
 }
+// ------------------------------------------
+// Iterate through an array with a for loop
+var myArray = [2,3,4,5,6];
+var total = 0;
 
-//console.log(myArray);
+for (var i = 0; i < myArray.length; i++) {
+  total += myArray[i];
+}
+
+//console.log(total);
