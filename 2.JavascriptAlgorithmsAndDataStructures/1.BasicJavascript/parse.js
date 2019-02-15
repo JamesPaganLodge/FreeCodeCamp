b1336a0fbf900d6b5348e4f444ff98e021c0b479
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-02-15 14:54:13 +0000

    parse an int with a radix (base number type)

--- 2.JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/parse.js
+++ 2.JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/parse.js
@@ -3,6 +3,15 @@ function convertToInteger(str) {
   return parseInt(str);
 }
 
-console.log(convertToInteger("56")); // 56
-console.log(convertToInteger("77")); // 77
-console.log(convertToInteger("JamesBond")); // NaN
+//console.log(convertToInteger("56")); // 56
+//console.log(convertToInteger("77")); // 77
+//console.log(convertToInteger("JamesBond")); // NaN
+// ----------------------------------------------
+// Parse Int with a radix (base numebr type)
+function convertToIntegerRadix(str) {
+  return parseInt(str, 2);
+}
+
+console.log(convertToIntegerRadix("10011")); // 19
+console.log(convertToIntegerRadix("111001")); // 57
+console.log(convertToIntegerRadix("JamesBond")); // NaN
