b083b4aa7dbcfe1016894fe5d4ea44b4fda77c44
jepl-towquest <james.pagan-lodge@towquest.co.uk>
2019-02-15 15:04:40 +0000

    Using multiple chained conditional operators

--- 2.JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/ternary.js
+++ 2.JavascriptAlgorithmsAndDataStructures/1.BasicJavascript/ternary.js
@@ -3,6 +3,15 @@ function checkEqual(a, b) {
   return a == b ? true : false;
 }
 
-console.log(checkEqual(1, 2));
-console.log(checkEqual(1, 1));
-console.log(checkEqual(1, -1));
+//console.log(checkEqual(1, 2));
+//console.log(checkEqual(1, 1));
+//console.log(checkEqual(1, -1));
+// ----------------------------------------------
+// Using multiple chained conditional (ternary) operators
+function checkSign(num) {
+  return num > 0 ? "positive" : num < 0 ? "negative" : "zero";
+}
+
+console.log(checkSign(10));
+console.log(checkSign(-12));
+console.log(checkSign(0));
