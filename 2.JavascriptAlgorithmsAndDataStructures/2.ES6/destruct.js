a2ef886e426aaf5c33e3fbaae8fd5ed4ffe501bb
JamesPaganLodge <stixx_60@hotmail.com>
2019-02-18 09:04:03 +0000

    Destructuring assignment to assign variables from nested objects

--- 2.JavascriptAlgorithmsAndDataStructures/2.ES6/destruct.js
+++ 2.JavascriptAlgorithmsAndDataStructures/2.ES6/destruct.js
@@ -7,8 +7,21 @@ const AVG_TEMPERATURE = {
 function getTempOfTmrw(avgTemperatures) {
   'use strict';
   const {tomorrow : tempOfTomorrow} = avgTemperatures;
-
   return tempOfTomorrow;
 }
 
-console.log(getTempOfTmrw(AVG_TEMPERATURE));
+//console.log(getTempOfTmrw(AVG_TEMPERATURE));
+// ----------------------------------------------
+// Destructuring assignment to assign variables from nested objects
+const LOCAL_FORECAST = {
+  today: { min: 72, max: 83 },
+  tomorrow: { min: 73.3, max: 84.6 }
+};
+
+function getMaxTempOfTmrw(forecast) {
+  'use strict';
+  const { tomorrow : { max : maxOfTomorrow } } = forecast;
+  return maxOfTomorrow;
+}
+
+console.log(getMaxTempOfTmrw(LOCAL_FORECAST));
