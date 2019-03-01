fc7407d53ee936141e190f52cd785f0a12dfc2b3
JamesPaganLodge <stixx_60@hotmail.com>
2019-03-01 15:24:27 +0000

    difference between fcc and browser consoles

--- 2.JavascriptAlgorithmsAndDataStructures/4.Debugging/consoledebug.js
+++ 2.JavascriptAlgorithmsAndDataStructures/4.Debugging/consoledebug.js
@@ -2,7 +2,14 @@
 let a = 5;
 let b = 1;
 a++
-console.log(a);
-
+//console.log(a);
 let sumAB = a + b;
-console.log(sumAB);
+//console.log(sumAB);
+// ----------------------------------------------
+// Understanding the difference between fcc and browser console
+let outputTwo = "This will print to the browser console 2 times";
+console.log(outputTwo);
+
+let outputOne = "Try to get this to log only once to the browser console";
+console.clear();
+console.log(outputOne);
