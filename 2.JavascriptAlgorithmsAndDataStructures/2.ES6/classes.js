5d725dd6c32da1d11659254f81bd58482e817a91
JamesPaganLodge <stixx_60@hotmail.com>
2019-03-01 10:42:23 +0000

    Use getters and setters to control access to an objects

--- 2.JavascriptAlgorithmsAndDataStructures/2.ES6/classes.js
+++ 2.JavascriptAlgorithmsAndDataStructures/2.ES6/classes.js
@@ -12,3 +12,28 @@ function makeClass() {
 const Vegetable = makeClass();
 const carrot = new Vegetable('carrot');
 //console.log(carrot.name); // should be 'carrot'
+// ----------------------------------------------
+// Use getters and setters to controla access to an object
+function makeClass2() {
+  "use strict";
+  class Thermostat {
+    constructor(temp) {
+      this.farenheit = temp;
+    }
+    get temperature() {
+      return 5 / 9 * (this.farenheit - 32);
+    }
+    set temperature(temp) {
+      this.farenheit = temp * 9.0 / 5 + 32;
+    }
+  }
+  return Thermostat;
+}
+
+const Thermostat = makeClass2();
+const thermos = new Thermostat(76); // setting in farenheit scale
+let temp = thermos.temperature; // 24.44 in C
+console.log(temp);
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in C
+console.log(temp);
