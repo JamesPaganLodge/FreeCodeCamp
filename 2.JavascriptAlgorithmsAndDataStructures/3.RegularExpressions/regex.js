89720e11e2ba7c088475aa4c1935671021bc3a65
JamesPaganLodge <stixx_60@hotmail.com>
2019-03-01 11:30:58 +0000

    Match a literal string

--- 2.JavascriptAlgorithmsAndDataStructures/3.RegularExpressions/regex.js
+++ 2.JavascriptAlgorithmsAndDataStructures/3.RegularExpressions/regex.js
@@ -1,5 +1,11 @@
 // Using the test method
 let testString = "Hello World!";
 let testRegex = /Hello/;
-let result = testRegex.test(testString);
-console.log(result);
+let testResult = testRegex.test(testString);
+//console.log(testResult);
+// ----------------------------------------------
+// Match literal strings
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/;
+let waldoResult = waldoRegex.test(waldoIsHiding);
+console.log(waldoResult);
