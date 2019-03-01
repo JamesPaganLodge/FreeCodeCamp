c0401227b4ee574b4bf7a566eac1c04c7c1cbc85
JamesPaganLodge <stixx_60@hotmail.com>
2019-03-01 16:10:04 +0000

    Catch mixed usage of single and double quotes

--- 2.JavascriptAlgorithmsAndDataStructures/4.Debugging/consoledebug.js
+++ 2.JavascriptAlgorithmsAndDataStructures/4.Debugging/consoledebug.js
@@ -30,4 +30,8 @@ let netWorkingCapital = receivables - payables; //recievables - payable;
 // Catch unclosed parentheses, brackets, braces and quotes
 let myArray = [1,2,3]; //[1,2,3;
 let arraySum = myArray.reduce((previous, current) => previous + current);
-console.log(`Sum of array values is: ${arraySum}`);
+//console.log(`Sum of array values is: ${arraySum}`);
+// ----------------------------------------------
+// Catch mixed usage of single and double quotes
+let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
+console.log(innerHtml);
