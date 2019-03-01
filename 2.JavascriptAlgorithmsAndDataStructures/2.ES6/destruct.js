05f139a34b7634404b3d98eeffcd8ea2d85ca5b0
JamesPaganLodge <stixx_60@hotmail.com>
2019-03-01 09:33:41 +0000

    Destructuring assignment to pass an object as a function parameter

--- 2.JavascriptAlgorithmsAndDataStructures/2.ES6/destruct.js
+++ 2.JavascriptAlgorithmsAndDataStructures/2.ES6/destruct.js
@@ -47,5 +47,26 @@ function removeFirstTwo(list) {
 }
 
 const arr = removeFirstTwo(source);
-console.log(arr); // should be [3,4,5,6,7,8,9,10]
-console.log(source); // should be [1,2,3,4,5,6,7,8,9,10]
+//console.log(arr); // should be [3,4,5,6,7,8,9,10]
+//console.log(source); // should be [1,2,3,4,5,6,7,8,9,10]
+// ----------------------------------------------
+// Destructuring assignment to pass an object as a functions parameters
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+const half = (function() {
+  'use strict';
+
+  return function half({max, min}) {
+    return (max + min) / 2.0;
+  };
+})();
+
+console.log(stats); // should be object
+console.log(half(stats)); // should be 28.015
