6703720add863170f273bc40263c86af147099ab
JamesPaganLodge <stixx_60@hotmail.com>
2019-03-01 11:38:03 +0000

    Match a string literal with different possibilities

--- 2.JavascriptAlgorithmsAndDataStructures/3.RegularExpressions/regex.js
+++ 2.JavascriptAlgorithmsAndDataStructures/3.RegularExpressions/regex.js
@@ -8,4 +8,10 @@ let testResult = testRegex.test(testString);
 let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
 let waldoRegex = /Waldo/;
 let waldoResult = waldoRegex.test(waldoIsHiding);
-console.log(waldoResult);
+//console.log(waldoResult);
+// ----------------------------------------------
+// Match a string literal with different possibilities
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/;
+let petResult = petRegex.test(petString);
+console.log(petResult);
