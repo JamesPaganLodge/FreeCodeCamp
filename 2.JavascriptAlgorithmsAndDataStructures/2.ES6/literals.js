0187e4c84e3c5d9b7bebc996f2c77bbb4bd10432
JamesPaganLodge <stixx_60@hotmail.com>
2019-03-01 09:37:25 +0000

    initial commit

---
+++ 2.JavascriptAlgorithmsAndDataStructures/2.ES6/literals.js
@@ -0,0 +1,16 @@
+// Create strings using template literals
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"]
+};
+
+function makeList(arr) {
+  "use strict";
+
+  const resultDisplayarray = null;
+
+  return resultDisplayarray;
+}
+
+const resultDisplayarray = makeList(result.failure);
