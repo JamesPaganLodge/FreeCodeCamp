89e44517e7e3672c0f92181ccec2629eaca41eaa
JamesPaganLodge <stixx_60@hotmail.com>
2019-03-15 14:07:03 +0000

    Add items to an array with push() and unshift()

--- 2.JavascriptAlgorithmsAndDataStructures/5.BasicDataStructures/array.js
+++ 2.JavascriptAlgorithmsAndDataStructures/5.BasicDataStructures/array.js
@@ -4,4 +4,13 @@ let yourArray = ["one", 2, false, "another string", 100]; // change this line
 // Access an array's contents using bracket notation
 let bracketArray = ["a", "b", "c", "d"];
 bracketArray[1] = "not b";
-console.log(bracketArray);
+//console.log(bracketArray);
+// ----------------------------------------------
+// Add items to an array with push() and unshift()
+function mixedNumbers(arr) {
+  arr.unshift("I", 2, "three");
+  arr.push(7, "VIII", 9);
+  return arr;
+}
+
+console.log(mixedNumbers(["IV", 5, "six"]));
