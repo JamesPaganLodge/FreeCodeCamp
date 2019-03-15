62c31b8b0f110d7e72aaf3d44643f55e2b2895a9
JamesPaganLodge <stixx_60@hotmail.com>
2019-03-15 14:10:48 +0000

    Remove items from an array with pop() and shift()

--- 2.JavascriptAlgorithmsAndDataStructures/5.BasicDataStructures/array.js
+++ 2.JavascriptAlgorithmsAndDataStructures/5.BasicDataStructures/array.js
@@ -13,4 +13,14 @@ function mixedNumbers(arr) {
   return arr;
 }
 
-console.log(mixedNumbers(["IV", 5, "six"]));
+//console.log(mixedNumbers(["IV", 5, "six"]));
+// ----------------------------------------------
+// Remove items from an array with pop() and shift()
+function popShift(arr) {
+  let popped = arr.pop();
+  let shifted = arr.shift();
+
+  return [shifted, popped];
+}
+
+console.log(popShift(["challenge", "is", "not", "complete"]));
