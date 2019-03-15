ade678ab801fec7cabd60445e0df9f9f36e9e0d0
JamesPaganLodge <stixx_60@hotmail.com>
2019-03-15 16:40:00 +0000

    initial commit

---
+++ 2.JavascriptAlgorithmsAndDataStructures/6.BasicAlgorithmScripting/longest.js
@@ -0,0 +1,8 @@
+// Find the longest word in a string
+function findLongestWordLength(str) {
+  
+
+  return str.length;
+}
+
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
