d8d880da4953f1277fb5d2d17ecde5fd3334b2ee
JamesPaganLodge <stixx_60@hotmail.com>
2019-03-15 13:53:11 +0000

    Access an array's contents using bracket notation

--- 2.JavascriptAlgorithmsAndDataStructures/5.BasicDataStructures/array.js
+++ 2.JavascriptAlgorithmsAndDataStructures/5.BasicDataStructures/array.js
@@ -1,2 +1,7 @@
 // Use an array to store a collection of data
 let yourArray = ["one", 2, false, "another string", 100]; // change this line
+// ----------------------------------------------
+// Access an array's contents using bracket notation
+let bracketArray = ["a", "b", "c", "d"];
+bracketArray[1] = "not b";
+console.log(bracketArray);
