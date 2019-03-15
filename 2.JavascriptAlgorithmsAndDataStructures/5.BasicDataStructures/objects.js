32e1e8a8b88f7180e51042b36120f5125f54db9d
JamesPaganLodge <stixx_60@hotmail.com>
2019-03-15 15:22:55 +0000

    Modify an object nested within an object

--- 2.JavascriptAlgorithmsAndDataStructures/5.BasicDataStructures/objects.js
+++ 2.JavascriptAlgorithmsAndDataStructures/5.BasicDataStructures/objects.js
@@ -9,4 +9,18 @@ foods.bananas = 13;
 foods.grapes = 35;
 foods.strawberries = 27;
 
-console.log(foods);
+//console.log(foods);
+// ----------------------------------------------
+// Modify an object nested within an object
+let userActivity = {
+  id: 23894201352,
+  date: "January 1, 2017",
+  data: {
+    totalUsers: 51,
+    online: 42
+  }
+};
+
+userActivity.data.online = 45;
+
+console.log(userActivity);
