52b6ec391cc66cb16b7dd1f1073c2fa5426e9c5d
JamesPaganLodge <stixx_60@hotmail.com>
2019-03-15 13:33:47 +0000

    Use caution when reinitializing variables inside a loop

--- 2.JavascriptAlgorithmsAndDataStructures/4.Debugging/consoledebug.js
+++ 2.JavascriptAlgorithmsAndDataStructures/4.Debugging/consoledebug.js
@@ -78,8 +78,26 @@ function countToFive() {
 
   //for (let i = 1; i <= len; i++) {
   for (let i = 0; i < len; i++) {
-    console.log(firstFive[i]);
+    //console.log(firstFive[i]);
   }
 }
 
 countToFive();
+// ----------------------------------------------
+// Use caution when reinitializing variables inside a loop
+function zeroArray(m, n) {
+  let newArray = [];
+  //let row = [];
+
+  for (let i = 0; i < m; i++) {
+    let row = [];
+    for (let j = 0; j < n; j++) {
+      row.push(0);
+    }
+    newArray.push(row);
+  }
+  return newArray;
+}
+
+let matrix = zeroArray(3, 2);
+console.log(matrix);
