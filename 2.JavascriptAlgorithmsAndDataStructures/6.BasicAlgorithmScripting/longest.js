d8e902fefddc9c48bb65a3329d6836984650b2c3
JamesPaganLodge <stixx_60@hotmail.com>
2019-03-15 17:02:38 +0000

    find the longest word in a string

--- 2.JavascriptAlgorithmsAndDataStructures/6.BasicAlgorithmScripting/longest.js
+++ 2.JavascriptAlgorithmsAndDataStructures/6.BasicAlgorithmScripting/longest.js
@@ -1,8 +1,15 @@
 // Find the longest word in a string
 function findLongestWordLength(str) {
-  
+  let arr = [];
+  let length = 0;
+  arr = str.split(" ");
 
-  return str.length;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length > length)
+      length = arr[i].length;
+  }
+
+  return length;
 }
 
 console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
